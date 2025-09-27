ac29760b3ed8303d07710460e5c9066524ad6938
harisahmed18403 <harisahmed18403@gmail.com>
2025-09-27 15:17:28 +0100

    added transition and generic classes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <TheHeader />
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </RouterView>
   </div>
 </template>
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { Transition } from 'vue'
 import TheHeader from '@/components/TheHeader.vue'
 </script>
